c46662eba38c349de8ba3b85bc9df66f0a846fe9
AlvinElves <hong3842@gmail.com>
2023-02-06 21:50:51 +0000

    Initial commit

---
+++ Practical/Practical 1/Charger.py
@@ -0,0 +1,16 @@
+import random
+
+
+class Charger:
+    def __init__(self, namep):
+        self.centreX = random.randint(100, 1000)
+        self.centreY = random.randint(100, 750)
+        self.name = namep
+
+    def draw(self, canvas):
+        body = canvas.create_oval(self.centreX - 10, self.centreY - 10,
+                                  self.centreX + 10, self.centreY + 10,
+                                  fill="black", tags=self.name)
+
+    def getLocation(self):
+        return self.centreX, self.centreY
---
+++ Practical/Practical 1/simpleBot.py
@@ -0,0 +1,66 @@
+import tkinter as tk
+
+from Bot import *
+
+
+def initialise(window):
+    window.resizable(False, False)
+    window.title('Robot')
+    window_Width = window.winfo_screenwidth()
+    window_Height = window.winfo_screenheight()
+    app_Width = 1100
+    app_Height = 850
+
+    x = int((window_Width / 2) - (app_Width / 2))
+    y = int((window_Height / 2) - (app_Height / 2)) - 40
+
+    window.geometry("{}x{}+{}+{}".format(app_Width, app_Height, x, y))
+    canvas = tk.Canvas(window, width=app_Width, height=app_Height)
+    canvas.pack()
+    return canvas
+
+
+def register(canvas):
+    registryActives = []
+    registryPassives = []
+    noOfBots = 3
+    noOfLights = 1
+    noOfCharger = 1
+    noOfHeater = 5
+    for i in range(0, noOfBots):
+        bot = Bot("Bot" + str(i))
+        registryActives.append(bot)
+        bot.draw(canvas)
+    for i in range(0, noOfLights):
+        lamp = Lamp("Lamp" + str(i))
+        registryPassives.append(lamp)
+        lamp.draw(canvas)
+    for i in range(0, noOfCharger):
+        charger = Charger("Charger" + str(i))
+        registryPassives.append(charger)
+        charger.draw(canvas)
+    for i in range(0, noOfHeater):
+        heater = Heater("Heater" + str(i))
+        registryPassives.append(heater)
+        heater.draw(canvas)
+    return registryActives, registryPassives
+
+
+def moveIt(canvas, registryActives, registryPassives):
+    for rr in registryActives:
+        lightIntensityL, lightIntensityR = rr.senseLight(registryPassives)
+        heatIntensityL, heatIntensityR = rr.senseHeat(registryPassives)
+        rr.brain(lightIntensityL, lightIntensityR, heatIntensityL, heatIntensityR)
+        rr.update(canvas, 1.0)
+    canvas.after(50, moveIt, canvas, registryActives, registryPassives)
+
+
+def main():
+    window = tk.Tk()
+    canvas = initialise(window)
+    registryActives, registryPassives = register(canvas)
+    moveIt(canvas, registryActives, registryPassives)
+    window.mainloop()
+
+
+main()
